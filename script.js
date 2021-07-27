03e9fa7b162f8b981d7cf8a7b5bbec4fe04546e1
Melanie Gaddis <mellow1984@charter.net>
2021-07-26 21:15:02 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(responseJson) {
+            let myAstronautDiv = document.getElementById("container");
+            let content = ""
+            responseJson.sort(function(astronaut1, astronaut2){return astronaut2.hoursInSpace - astronaut1.hoursInSpace});
+            for (let index = 0; index < responseJson.length; index++) {
+                let cssClass = ""
+                if (responseJson[index].active === true) {
+                    cssClass = "active"
+                }
+                content += `
+                <div class="astronaut">
+                <h3>${responseJson[index].firstName} ${responseJson[index].lastName}</h3>
+                <ul>
+                <li> Hours in Space: ${responseJson[index].hoursInSpace}</li>
+                <li class = "${cssClass}"> Active: ${responseJson[index].active}</li>
+                <li> Skills: ${responseJson[index].skills}</li>
+                </ul>
+                <img class="avatar" src="${responseJson[index].picture}">
+                </div>
+                `
+
+                console.log(responseJson[index].firstName);
+            }
+            myAstronautDiv.innerHTML = content + `<p>Astronaut Count: ${responseJson.length}</p>`
+            
+        });
+    });
+});
